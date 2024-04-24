0b2826909c9bb0e66d93726a6107d2c908b2c6a2
Pradhyumnmitt <166858319+Pradhyumn-mittal@users.noreply.github.com>
2024-04-24 12:18:20 +0530

    initial commit

---
+++ src/main/java/com/java/weatherfetch/WeatherFetchApplication.java
@@ -0,0 +1,13 @@
+package com.java.weatherfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherFetchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherFetchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/java/weatherfetch/WeatherFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.weatherfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFetchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
